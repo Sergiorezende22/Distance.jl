780704921617d187a60b87c3e0291f9634de601c
Sergio Felipe Rezende do Nascimento <sergiorezende2002@gmail.com>
2020-06-29 16:41:58 -0300

    Iniciating on tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,20 @@ using Distance
 using Test
 
 @testset "Distance.jl" begin
-    
+    #=
+    x = [2,2]
+    y = [2,4]
+    @test calculate(Euclidean(), x, y) == 2
+
+    @test calculate(CityBlock(), 2, 1) ==
+    @test calculate(TotalVariation(), 2, 1) ==
+    @test calculate(Chebyshev(), 2, 1) ==
+    @test calculate(Jaccard(), 2, 1) ==
+    @test calculate(BrayCurtis(), 2, 1) ==
+    @test calculate(CosineDist(), 2, 1) ==
+    @test calculate(ChiSqDist(), 2, 1) ==
+    @test calculate(SpanNormDist(), 2, 1) ==
+=#
+
 
 end
